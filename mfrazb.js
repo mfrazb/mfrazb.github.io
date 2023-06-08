de1aa178b9e4e8745a6e601b6f9d23f90239278d
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-06-08 18:44:55 -0400

    Update mouseover and add project arrow on mouseover

--- mfrazb.js
+++ mfrazb.js
@@ -1,18 +1,23 @@
 // when user mouses over projects, update right side background image to corresponding project
 
-
-// const mainImage = document.querySelector(".main-image");
-// mainImage.style.backgroundImage = "url(images/rock-paper-scissors-screenshot.png)";
-
 // invoke callback when user mouses over project
 function changeBackground(event) {
     const rightSideImage = document.querySelector(".main-image");
     if (event.target.id === "fraziers") {
+        // update image to Frazier's landing page 
         rightSideImage.style.backgroundImage = "url(images/fraziers-finer-flavors-screenshot-half-width.png)";
+        
+        // show arrow next to Frazier's description pointing to image
+        const arrow = document.createElement("div");
+        arrow.textContent = ">";
+        arrow.classList.add("arrow");
+        event.target.appendChild(arrow);  
     }
     if (event.target.id === "rock-paper-scissors") {
-        console.log(event.target.id);
         rightSideImage.style.backgroundImage = "url(images/rock-paper-scissors-screenshot.png)";
+    }
+    if (event.target.id === "fancy-shmancy") {
+        rightSideImage.style.backgroundImage = "url(images/hiplab-screenshot.png)";
     }
 }
 
